c474b79794e96f3fdaf35053d8a5c7a939678cba
Shifuwu123 <nicozone123@gmail.com>
2024-06-12 00:22:35 -0400

    Importacion de fuentes

---
+++ fonts.py
@@ -0,0 +1,5 @@
+import flet as ft
+
+def load_custom_fonts():
+    ft.font.register("Holtwood One SC", "assets/fonts/HoltwoodOneSC-Regular.ttf")
+    ft.font.register("Inter", "assets/fonts/Inter-Regular.ttf")
